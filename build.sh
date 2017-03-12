282fc1ef5c9378cf3444a5939d46a0451c774252
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-03-12 11:01:11 -0700

    include confd binary

--- build.sh
+++ build.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-if [[ ! -f confd ]]; then
-    wget https://github.com/kelseyhightower/confd/releases/download/v0.11.0/confd-0.11.0-linux-amd64
-    mv confd-0.11.0-linux-amd64 confd
-fi
 docker build -t miguelgrinberg/easy-lb .
